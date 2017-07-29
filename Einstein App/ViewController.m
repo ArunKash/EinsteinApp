18af9b5194943914a6bb6b25644e799210fa960a
arunKash <arun.prakash@cognizant.com>
2017-07-30 01:57:10 +0530

    Added Animations to Initial View
    Animate With Duration and stuff.

--- Einstein App/ViewController.m
+++ Einstein App/ViewController.m
@@ -10,11 +10,19 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *Button;
+@property (weak, nonatomic) IBOutlet UIImageView *camImage;
+@property (weak, nonatomic) IBOutlet UILabel *visionLabel;
+
+
 - (IBAction)VisionBtn:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *emotionBtn;
+@property (weak, nonatomic) IBOutlet UIImageView *emotionIcon;
+@property (weak, nonatomic) IBOutlet UILabel *emotionLabel;
+
 @property (weak, nonatomic) IBOutlet UIButton *predictBtn;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *EmotionLeading;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftMove;
+@property (weak, nonatomic) IBOutlet UIImageView *predictIcon;
+@property (weak, nonatomic) IBOutlet UILabel *predictLabel;
+
 
 @end
 
@@ -22,13 +30,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self hideAllButtons];
     [self applyButtonStyle:self.Button];
     [self applyButtonStyle:self.emotionBtn];
     [self applyButtonStyle:self.predictBtn];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:YES];
+    [self showAnimatedStyle2];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -50,5 +62,107 @@
     button.layer.shadowRadius = 4;
     button.layer.shadowOffset = CGSizeMake(4.0f, 4.0f);
 
+}
+-(void)hideAllButtons {
+    
+    self.emotionIcon.alpha = 0.0f;
+    self.predictIcon.alpha = 0.0f;
+    self.camImage.alpha = 0.0f;
+    self.visionLabel.alpha = 0.0f;
+    self.emotionLabel.alpha = 0.0f;
+    self.predictLabel.alpha = 0.0f;
+    self.Button.alpha = 0.0f;
+    self.emotionBtn.alpha = 0.0f;
+    self.predictBtn.alpha =0.0f;
+}
+
+-(void)showAnimatedStyle1 {
+    
+    [UIView animateWithDuration:0.5f
+                          delay:0.5f
+                        options:UIViewAnimationOptionAllowUserInteraction
+                     animations:^{
+                         //First
+                         
+                         self.Button.alpha = 1.0f;
+                         self.emotionBtn.alpha = 1.0f;
+                         self.predictBtn.alpha =1.0f;
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.5f
+                                               delay:0.5f
+                                             options:UIViewAnimationOptionAllowUserInteraction
+                                          animations:^{
+                                              //Secound
+                                              self.emotionIcon.alpha = 1.0f;
+                                              self.predictIcon.alpha = 1.0f;
+                                              self.camImage.alpha = 1.0f;
+                                              
+                                          }
+                                          completion:^(BOOL finished) {
+                         [UIView animateWithDuration:1.5f
+                                          animations:^{
+                                             //Third
+                                              self.visionLabel.alpha = 1.0f;
+                                              self.emotionLabel.alpha = 1.0f;
+                                              self.predictLabel.alpha = 1.0f;
+                                          }];
+                                          }];
+                     }];
+}
+
+-(void)showAnimatedStyle2 {
+    
+    [UIView animateWithDuration:0.5f
+                          delay:0.5f
+                        options:UIViewAnimationOptionAllowUserInteraction
+                     animations:^{
+                         //First
+                         
+                         
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.5f
+                                               delay:0.5f
+                                             options:UIViewAnimationOptionAllowUserInteraction
+                                          animations:^{
+                                              //Secound
+                                              
+                                              self.Button.alpha = 1.0f;
+                                              self.camImage.alpha = 1.0f;
+                                              self.visionLabel.alpha = 1.0f;
+
+                                            
+                                              
+                                          }
+                                          completion:^(BOOL finished) {
+                                              
+                                              //start
+                                              
+                                              [UIView animateWithDuration:0.5f
+                                                                    delay:0.5f
+                                                                  options:UIViewAnimationOptionAllowUserInteraction
+                                                               animations:^{
+                                                                   //third
+                                                                   self.emotionIcon.alpha = 1.0f;
+                                                                   self.emotionBtn.alpha = 1.0f;
+                                                                   self.emotionLabel.alpha = 1.0f;
+                                                               }
+                                                               completion:^(BOOL finished) {
+                                                                   [UIView animateWithDuration:1.5f
+                                                                                    animations:^{
+                                                                                        //Fourth
+                                                                                        self.predictBtn.alpha = 1.0f;
+                                                                                        self.predictIcon.alpha = 1.0f;
+                                                                                        self.predictLabel.alpha = 1.0f;
+                                            
+                                                                                    }];
+                                                               }];
+                                              
+                                              //here
+                                              
+                                              
+                                          }];
+                     }];
 }
 @end
